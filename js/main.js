68402c7fc3e5852b2b46bf41c7d9b94638bcca9c
dilan <medinadilan07gmail.com>
2026-05-29 23:52:11 -0400

    avance eventos index

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 
 const miBoton = document.getElementById("iboton"); //conectamos el html con js
+const iconoluna= document.getElementById("icono-luna");
+const tarjeta= document.querySelectorAll(".tarjeta");
 
-// 1. Ir arriba al hacer clic
+
+
+//Evento subir hacia arriba
+
 miBoton.addEventListener("click", () => {     //addeventliterner es como ponerle un microfono
     window.scrollTo({ top: 0, behavior: "smooth" }); //hasta arriba, smooth deslizamiento suave
 });
@@ -13,7 +18,49 @@ window.addEventListener("scroll", () => {
     // Si es 0, se oculta por completo. Si es mayor a 200, se muestra.
     if (scrollActual === 0) {
         miBoton.classList.remove("mostrar-boton");
-    } else if (scrollActual > 100) {
+    } else if (scrollActual > 400) {
         miBoton.classList.add("mostrar-boton");
     }
 });
+
+
+
+//cambiar el color del body
+
+iconoluna.addEventListener("click", (evento) => {
+    document.body.classList.toggle("body-oscuro");
+    console.log(iconoluna);
+    evento.preventDefault(); //evita que la pagina recargue por el href
+
+    // 2. Corregido: Usamos "bi-moon-stars-fill" que es la clase real de tu HTML
+    if (document.body.classList.contains("body-oscuro")) {
+        // Al entrar a modo oscuro, reemplazamos la luna con estrellas por el sol
+        iconoluna.classList.replace("bi-moon-stars-fill", "bi-brightness-high-fill");
+    } else {
+        // Al salir, reemplazamos el sol de vuelta por la luna con estrellas
+        iconoluna.classList.replace("bi-brightness-high-fill", "bi-moon-stars-fill");
+    }     
+});
+
+
+
+//cambiar de imagen en las cards
+
+tarjeta.forEach((img) => { // 2. Recorres cada imagen de la lista una por una
+    const imgOriginal = img.src;
+    const imgNueva = img.getAttribute("data-img2");
+
+    // 3. Evento: Cuando el mouse entra a la imagen
+    img.addEventListener("mouseenter", () => {
+        if (imgNueva) { //verifica si la imgnueva tiene el atributo o la ruta
+            img.src = imgNueva; 
+        }
+    });
+
+    // 4. Evento: Cuando el mouse sale de la imagen
+    img.addEventListener("mouseleave", () => {
+        img.src = imgOriginal;
+    });
+});
+
+
